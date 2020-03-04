087b43513fba9b25ec4db42b84815c28623652e4
Adrian Reyes <adriandmc05@gmail.com>
2020-03-03 20:00:06 -0400

    Monto mayor a la denominacion mas alta

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 function calcu(denominaciones, monedas, factura) {
-  let pago_mayor = false;
-  let faltante_pagar = factura
-  let cant_monedas = 0
+  let faltante_pagar = factura;
+  let cant_monedas = 0;
   let vuelto;
-  let prom;
+  let aux_promedio = 0;
+  let promedio = 0;
   let acum = 0;
 
-
   if (faltante_pagar >= monedas[denominaciones - 1]) {
-    // si el monto es mayor a la denominacion mas alta
-    for (i = denominaciones - 1; i >= 0; i--) {
-      if (faltante_pagar >= monedas[i]) {
-        
-        cant_monedas++;
-        acum = acum + monedas[i];
-        
-        
-        console.log(acum);
-        
-        if (acum >= faltante_pagar) {
 
-          console.log("pago con " + cant_monedas + " monedas Un monto de " + acum + " Suma de monedas ");
-          vuelto = acum - faltante_pagar;
-          break;
-        }else {
-          i = denominaciones;
-        }
+    // si el monto es mayor a la denominacion mas alta
+    while (faltante_pagar > monedas[denominaciones - 1]) {
+      console.log("pago con " + monedas[denominaciones - 1]);
+      faltante_pagar = faltante_pagar - monedas[denominaciones - 1];
+      console.log("falta pagar " + faltante_pagar);
+      acum = acum + monedas[denominaciones - 1];
+      cant_monedas++;
+    }
+
+    for (i = 0; i < denominaciones; i++) {
+      if (faltante_pagar <= monedas[i]) {
+        acum = acum + monedas[i];
+        console.log("pago con " + monedas[i]);
+        cant_monedas++;
+        vuelto = acum - factura;
+        break;
       }
     }
   } else {
@@ -33,6 +31,7 @@ function calcu(denominaciones, monedas, factura) {
     for (i = 0; i < denominaciones; i++) {
       if (faltante_pagar <= monedas[i]) {
         console.log("pago con " + monedas[i]);
+        acum = acum + monedas[i];
         cant_monedas++;
         vuelto = monedas[i] - factura;
         break;
@@ -40,13 +39,11 @@ function calcu(denominaciones, monedas, factura) {
     }
   }
 
-
   faltante_devolver = vuelto;
 
   //para el vuelto exacto
   for (i = denominaciones - 1; i >= 0; i--) {
     while (faltante_devolver >= monedas[i]) {
-
       acum = acum + monedas[i];
       console.log("falta devolver " + faltante_devolver);
       faltante_devolver = faltante_devolver - monedas[i];
@@ -55,17 +52,17 @@ function calcu(denominaciones, monedas, factura) {
     }
   }
 
-  prom = (acum/cant_monedas);
+  aux_promedio = acum / cant_monedas;
+  promedio = aux_promedio.toFixed(2);
 
-  return {cant_monedas , prom};
 
-
+  return { cant_monedas, promedio };
 }
 
 let denominaciones = 6;
 let monedas = [1, 2, 5, 10, 20, 50];
 let factura = 90;
 
-let salida =  calcu(denominaciones, monedas, factura);
+let salida = calcu(denominaciones, monedas, factura);
 
-console.log("Promedio " + salida.prom + " monedas utilizadas " + salida.cant_monedas);
+console.log("Promedio " + salida.promedio + " monedas utilizadas " + salida.cant_monedas);
